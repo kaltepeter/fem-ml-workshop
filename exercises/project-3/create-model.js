51e2f06b35af9d672ed20cf15261ddc5360ff24a
kaltepeter <kayla.altepeter@datasite.com>
2024-04-19 12:53:23 -0500

    feat(project-3): finished

---
+++ exercises/project-3/create-model.js
@@ -0,0 +1,56 @@
+const tf = require("@tensorflow/tfjs");
+
+const kernelSize = [3, 3];
+const filter = 32;
+const dropout_conv = 0.3;
+const dropout_dense = 0.3;
+const numClasses = 2;
+
+const model = tf.sequential();
+
+model.add(
+  tf.layers.conv2d({
+    inputShape: [28, 28, 4],
+    filters: filter,
+    kernelSize,
+    activation: 'relu'
+  })
+);
+
+model.add(
+  tf.layers.conv2d({
+    filters: filter,
+    kernelSize,
+    activation: "relu",
+  })
+);
+
+model.add(tf.layers.maxPooling2d({
+  poolSize: [2,2],
+}));
+
+model.add(tf.layers.dropout({ rate: dropout_conv }));
+
+model.add(tf.layers.flatten());
+
+model.add(tf.layers.dense({
+  units: 10,
+  activation: 'relu'
+}));
+
+model.add(tf.layers.dropout({ rate: dropout_dense }));
+
+model.add(tf.layers.dense({
+  units: numClasses,
+  activation: 'softmax'
+}));
+
+
+const optimizer = tf.train.adam(0.0001);
+model.compile({
+  optimizer,
+  loss: 'categoricalCrossentropy',
+  metrics: ['accuracy']
+});
+
+module.exports = model;
